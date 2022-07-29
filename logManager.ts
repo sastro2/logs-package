065b766e29a407b2a4a4d174461b86f35b31386c
Sascha Stropek <sascha.stropek@gmail.com>
2022-07-29 20:00:21 +0200

    Changed type error logs send all logs immediately and added todos

--- logManager.ts
+++ logManager.ts
@@ -15,14 +15,20 @@ export class LogManager {
 
   addLog(log: Log) {
     if (!(log.logType in logTypes)) {
+      // TODO
       // check if log needs to be sent immediately and proceed accordingly
+      // if yes add the log and send it otherwhise just add it
       console.log('custom log type', log.logType);
       return;
     }
 
     if (log.logType === 'error') {
+      // TODO
       // built in log types that need to be sent immediately
+      // adds log and sends it without user having to call sendLogs method
       console.log('send immediately');
+      this.logs.push(log);
+      this.sendLogs();
       return;
     }
 
@@ -31,7 +37,9 @@ export class LogManager {
     console.log(log, 'added');
   }
 
-  // send all unsent logs
+  // TODO
+  // generate id and timestamp and send all unsent logs
+  // request body should contain id, timestamp, and logs array as json
   sendLogs() {
     console.log(this.logs, 'sent');
     this.logs = [];
